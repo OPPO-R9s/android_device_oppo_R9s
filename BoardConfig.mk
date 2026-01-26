b7b319cb7b9cca4b7d11087bd5d8adf4801ce849
Ricky Cheung <rcheung844@gmail.com>
2026-01-26 23:55:25 +0800

    R9s: Specify using oem as vendor
    * Needed for SEPolicy back in common tree.
    
    Change-Id: Ic671bb22e69e402589cce385652528311541909f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,9 @@ TARGET_BOOTLOADER_BOARD_NAME := msm8953
 # Platform
 TARGET_BOARD_PLATFORM := msm8953
 
+# Partitions
+TARGET_VENDOR_PARTITION := oem
+
 # Inherit from common device tree
 include device/oppo/thortanium-common/BoardConfigCommon.mk
 
